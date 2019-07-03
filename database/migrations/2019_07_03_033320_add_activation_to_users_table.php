6b693fa667572c49c5f0f759a6a0232092790cae
little_monkey <1303354465@qq.com>
2019-07-03 15:17:37 +0800

    用户激活

--- database/migrations/2019_07_03_033320_add_activation_to_users_table.php
+++ database/migrations/2019_07_03_033320_add_activation_to_users_table.php
@@ -4,29 +4,21 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddIsAdminToUsersTable extends Migration
+class AddActivationToUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('is_admin')->default(false);
+            $table->string('activation_token')->nullable();
+            $table->boolean('activated')->default(false);
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('is_admin');
+            $table->dropColumn('activation_token');
+            $table->dropColumn('activated');
         });
     }
 }
