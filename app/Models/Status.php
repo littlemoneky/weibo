fa00ea82d52e3ed48dfbd7720002857d488749db
little_monkey <1303354465@qq.com>
2019-07-04 12:59:42 +0800

    发布微博

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -4,9 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-
 class Status extends Model
 {
+    protected $fillable = ['content'];
+
     public function user()
     {
         return $this->belongsTo(User::class);
